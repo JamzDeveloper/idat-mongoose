bef65010b45cb2f0ceedfd710130876528db71ee
jmontenegro <jmontenegro@unifiedsoftware.us>
2023-08-26 13:29:40 -0500

    added express-session

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,9 @@ const path = require("path");
 
 const express = require("express");
 const mongoose = require("mongoose");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+
 const { Server: SocketServer } = require("socket.io");
 
 const categoryRoutes = require("../modules/categories/routes");
@@ -10,6 +13,7 @@ const ProductRoutes = require("../modules/product/routes/");
 const UserPath = require("../modules/user/routes");
 const GroupRoutes = require("../modules/group/routes");
 const { createMessage } = require("../modules/group/services/group.service");
+const { checkSession } = require("../middleware/checkSession.middleware");
 
 class Server {
   constructor() {
@@ -20,6 +24,7 @@ class Server {
     this.productPath = "/product";
     this.userPath = "/user";
     this.groupPath = "/group";
+    this.sessionExpressPath = "/login";
     this.server = http.createServer(this.app);
     this.io = new SocketServer(this.server, {
       cors: {
@@ -46,12 +51,58 @@ class Server {
 
     //parse and reading of body
     this.app.use(express.json());
+
+    this.app.use(express.urlencoded({ extended: true }));
+
+    this.app.use(
+      session({
+        store: new FileStore({ path: "./sessions" }),
+        secret: "mysecretkey",
+        resave: false,
+        saveUninitialized: true,
+      })
+    );
   }
   routes() {
     this.app.use(this.originPath + this.categoryPath, categoryRoutes);
     this.app.use(this.originPath + this.productPath, ProductRoutes);
     this.app.use(this.originPath + this.userPath, UserPath);
     this.app.use(this.originPath + this.groupPath, GroupRoutes);
+
+    this.app.get(this.sessionExpressPath, (req, res) => {
+      res.sendFile(path.join(__dirname, "../public/login/index.html"));
+    });
+
+    this.app.post("/session_login2", (req, res) => {
+      /// obtener datos
+
+      const { user, password } = req.body;
+
+      ///consultar a la base de datos
+      const usuario = {
+        name: "jose",
+        username: "jose_user",
+        password: "jose_password",
+      };
+
+      //traer usuario, comparar contraseña
+
+      //comprar contraseña
+      if (usuario.username == user && usuario.password == password) {
+        //agregar sesion al server
+        req.session.isLoggedIn = true;
+        req.session.name = usuario.name;
+
+        //redireccionar
+        res.redirect("/home");
+      } else {
+        res.redirect("/login");
+      }
+    });
+
+    this.app.get("/home", checkSession, (req, res) => {
+      res.sendFile(path.join(__dirname, "../public/home/index.html"));
+    });
   }
 
   socket() {
